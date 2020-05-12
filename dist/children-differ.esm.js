06d8cb3f9f48c8ef59cfed831e73b42e521701ae
daybrush <daybrush@gmail.com>
2020-05-12 13:47:45 +0900

    chore(release): Release 1.0.1

---
+++ dist/children-differ.esm.js
@@ -0,0 +1,143 @@
+/*
+Copyright (c) 2019-present NAVER Corp.
+name: @egjs/children-differ
+license: MIT
+author: NAVER Corp.
+repository: https://github.com/naver/egjs-children-differ
+version: 1.0.1
+*/
+import ListDiffer, { diff as diff$1 } from '@egjs/list-differ';
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+/* global Reflect, Promise */
+var extendStatics = function (d, b) {
+  extendStatics = Object.setPrototypeOf || {
+    __proto__: []
+  } instanceof Array && function (d, b) {
+    d.__proto__ = b;
+  } || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+  };
+
+  return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+  extendStatics(d, b);
+
+  function __() {
+    this.constructor = d;
+  }
+
+  d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+/*
+egjs-children-differ
+Copyright (c) 2019-present NAVER Corp.
+MIT license
+*/
+var findKeyCallback = typeof Map === "function" ? undefined : function () {
+  var childrenCount = 0;
+  return function (el) {
+    return el.__DIFF_KEY__ || (el.__DIFF_KEY__ = ++childrenCount);
+  };
+}();
+
+/**
+ * A module that checks diff when child are added, removed, or changed .
+ * @ko 자식 노드들에서 자식 노드가 추가되거나 삭제되거나 순서가 변경된 사항을 체크하는 모듈입니다.
+ * @memberof eg
+ * @extends eg.ListDiffer
+ */
+
+var ChildrenDiffer =
+/*#__PURE__*/
+function (_super) {
+  __extends(ChildrenDiffer, _super);
+  /**
+   * @param - Initializing Children <ko> 초기 설정할 자식 노드들</ko>
+   */
+
+
+  function ChildrenDiffer(list) {
+    if (list === void 0) {
+      list = [];
+    }
+
+    return _super.call(this, list, findKeyCallback) || this;
+  }
+
+  return ChildrenDiffer;
+}(ListDiffer);
+
+/*
+egjs-children-differ
+Copyright (c) 2019-present NAVER Corp.
+MIT license
+*/
+/**
+ *
+ * @memberof eg.ChildrenDiffer
+ * @static
+ * @function
+ * @param - Previous List <ko> 이전 목록 </ko>
+ * @param - List to Update <ko> 업데이트 할 목록 </ko>
+ * @return - Returns the diff between `prevList` and `list` <ko> `prevList`와 `list`의 다른 점을 반환한다.</ko>
+ * @example
+ * import { diff } from "@egjs/children-differ";
+ * // script => eg.ChildrenDiffer.diff
+ * const result = diff([0, 1, 2, 3, 4, 5], [7, 8, 0, 4, 3, 6, 2, 1]);
+ * // List before update
+ * // [1, 2, 3, 4, 5]
+ * console.log(result.prevList);
+ * // Updated list
+ * // [4, 3, 6, 2, 1]
+ * console.log(result.list);
+ * // Index array of values added to `list`
+ * // [0, 1, 5]
+ * console.log(result.added);
+ * // Index array of values removed in `prevList`
+ * // [5]
+ * console.log(result.removed);
+ * // An array of index pairs of `prevList` and `list` with different indexes from `prevList` and `list`
+ * // [[0, 2], [4, 3], [3, 4], [2, 6], [1, 7]]
+ * console.log(result.changed);
+ * // The subset of `changed` and an array of index pairs that moved data directly. Indicate an array of absolute index pairs of `ordered`.(Formatted by: Array<[index of prevList, index of list]>)
+ * // [[4, 3], [3, 4], [2, 6]]
+ * console.log(result.pureChanged);
+ * // An array of index pairs to be `ordered` that can synchronize `list` before adding data. (Formatted by: Array<[prevIndex, nextIndex]>)
+ * // [[4, 1], [4, 2], [4, 3]]
+ * console.log(result.ordered);
+ * // An array of index pairs of `prevList` and `list` that have not been added/removed so data is preserved
+ * // [[0, 2], [4, 3], [3, 4], [2, 6], [1, 7]]
+ * console.log(result.maintained);
+ */
+
+function diff(prevList, list) {
+  return diff$1(prevList, list, findKeyCallback);
+}
+
+/*
+egjs-children-differ
+Copyright (c) 2019-present NAVER Corp.
+MIT license
+*/
+
+export default ChildrenDiffer;
+export { diff };
+//# sourceMappingURL=children-differ.esm.js.map
